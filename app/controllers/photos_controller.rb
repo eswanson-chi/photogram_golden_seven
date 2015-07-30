604837c4d012ca95bea9d59351093c37c28de670
eswanson-chi <ericswanson@wireless-s0-151-3-91.uchicago.edu>
2015-07-29 20:49:16 -0500

    functionally correct.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,44 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find(params["id"])
+
+    render("show.html.erb")
+  end
+
+  def new_photo_form
+    render("new_photo.html.erb")
+  end
+
+  def create_photo
+    p = Photo.new
+    p.caption = params["photo_caption"]
+    p.source = params["photo_source"]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def update_photo_form
+    @photo = Photo.find(params["id"])
+    render("update_photo.html.erb")
+  end
+
+  def update_photo
+    p = Photo.find(params["id"])
+    p.caption = params["update_caption"]
+    p.source = params["update_source"]
+    p.save
+
+    redirect_to("http://localhost:3000/photos/#{p.id}/show")
+  end
+
+  def destroy
+    p = Photo.find(params["id"])
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos/")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+ # Routes to CREATE photos
+  get("/photos/new",           { :controller => "photos", :action => "new_photo_form" })
+  get("/photos/create_photo",       { :controller => "photos", :action => "create_photo" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/:id/show",       { :controller => "photos", :action => "show" })
+
+   # Routes to UPDATE photos
+  get("/photos/:id/edit",           { :controller => "photos", :action => "update_photo_form" })
+  get("/photos/:id/update_photo",       { :controller => "photos", :action => "update_photo" })
+
+   # Routes to DELETE photos
+  get("/photos/delete_photo/:id",           { :controller => "photos", :action => "destroy" })
+
 
 end
